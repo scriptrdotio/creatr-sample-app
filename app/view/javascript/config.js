b6eafd5e0145476a95adb563631a109ed634a8f5
touta <marvette.feghali@elementn.com>
2020-07-13 14:45:58 +0300

    Change to relative path

--- app/view/javascript/config.js
+++ app/view/javascript/config.js
@@ -1,9 +1,3 @@
-var login = {
-   expiry:6,
-   loginApi: "/app/api/login/login",
-   validateTokenApi: "/app/api/login/validateToken"
-};
-
 var wssConfig = ["wsClientProvider",function (wsClientProvider) {
     wsClientProvider.setPublishChannel("requestChannel");
     var subscribeChannels = ["responseChannel"]
